8b8cb422f4ffdce06a6abf468cf215a02aa405a2
@menghui.w <menghui.w@affinidi.com>
2022-06-08 20:16:56 +0800

    Added FormData for calculator

--- src/screens/IpptCalculatorScreen.tsx
+++ src/screens/IpptCalculatorScreen.tsx
@@ -1,9 +1,23 @@
 import { StyleSheet } from "react-native";
+import { TextInput, Button, Switch, HelperText } from "react-native-paper";
+import {useForm, Controller, SubmitHandler} from 'react-hook-form';
 
 import EditScreenInfo from "../components/EditScreenInfo";
-import { Text, View } from "../components/Themed";
+import { Text, View, ScrollView } from "../components/Themed";
+
+type FormData = {
+  pushups: string
+  situps: string
+  runtime: string
+}
 
 export default function IpptCalculatorScreen() {
+  const { control, errors, formState, handleSubmit } = useForm<FormData>({
+    mode: "onChange",
+  })
+
+  const submit = (data) => console.log(data)
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>IPPT Calculator</Text>
