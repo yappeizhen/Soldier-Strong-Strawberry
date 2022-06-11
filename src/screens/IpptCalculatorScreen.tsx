e9949ed420e26b43a067557a965234abff82fb48
@menghui.w <menghui.w@affinidi.com>
2022-06-11 16:13:08 +0800

    Included score saving and score calculation

--- src/screens/IpptCalculatorScreen.tsx
+++ src/screens/IpptCalculatorScreen.tsx
@@ -1,19 +1,20 @@
-import { doc, onSnapshot, updateDoc } from "firebase/firestore";
+import { doc, onSnapshot, startAfter, updateDoc } from "firebase/firestore";
 import { useEffect } from "react";
 import { useState } from "react";
 import { StyleSheet } from "react-native";
-import { Card } from "react-native-paper";
+import { Card, DataTable } from "react-native-paper";
 
 import CustomButton from "../components/CustomButton";
 import CustomTextInput from "../components/CustomInputText";
 import CustomSnackBar from "../components/CustomSnackBar";
-import { ScrollView, Text, View } from "../components/Themed";
+import { ScrollView, View } from "../components/Themed";
 import { firebaseFirestore } from "../firebase/firebase";
 import { useAuthState } from "../hooks/useAuthState";
 import Colors from "../constants/Colors";
 import useColorScheme from "../hooks/useColorScheme";
 import EditScreenInfo from "../components/EditScreenInfo";
-import { FirestoreStaticStat, StaticStat } from "../constants/Models";
+import { FirestoreRunningStat, FirestoreStaticStat, RunningStat, StaticStat } from "../constants/Models";
+import { stringify } from "@firebase/util";
 
 
 
@@ -23,11 +24,17 @@ export default function IpptCalculatorScreen() {
 
   const [snackBarMessage, setSnackBarMessage] = useState<string>("");
   const [isSnackBarVisible, setIsSnackBarVisible] = useState<boolean>(false);
-  const [pushups, setPushups] = useState<number>();
-  const [situps, setSitups] = useState<number>();
-  const [runtime, setRuntime] = useState<number>();
-  const [age, setAge] = useState<number>();
-  const [totalScore, setTotalScore] = useState<number>();
+  const [pushups, setPushups] = useState<string>("0");
+  const [situps, setSitups] = useState<string>("0");
+  const [runtime, setRuntime] = useState<string>("1100");
+  const [runtimeMins, setRuntimeMins] = useState<string>((Math.floor(parseInt(runtime) / 60)).toString());
+  const [runtimeSecs, setRuntimeSecs] = useState<string>((parseInt(runtime) % 60).toString());
+  const [age, setAge] = useState<number>(18);
+  const [totalScore, setTotalScore] = useState<number>(0);
+  const [currentUser, setCurrentUser] = useState<any>();
+  const [pushupArray, setPushupArray] = useState<StaticStat[]>([]);
+  const [situpArray, setSitupArray] = useState<StaticStat[]>([]);
+  const [runtimeArray, setRuntimeArray] = useState<StaticStat[]>([]);
 
   const pushupTable = require('../constants/ippt/pushup.json');
   const situpTable = require('../constants/ippt/situp.json');
@@ -38,9 +45,38 @@ export default function IpptCalculatorScreen() {
   useEffect(() => {
     const unsubscribe = () => {
       if (user) {
+        setCurrentUser(user);
         const userProfileRef = doc(firebaseFirestore, "userProfiles", user.uid);
         onSnapshot(userProfileRef, (snapshot) => {
           if (snapshot.exists()) {
+            let pushupArrayTemp: StaticStat[] = [];
+            let situpArrayTemp: StaticStat[] = [];
+            let runtimeArrayTemp: RunningStat[] = [];
+            snapshot.data().pushups
+            ?.forEach((stat: FirestoreStaticStat) => {
+              pushupArrayTemp.push({
+                number: stat.number,
+                date: stat.date.toDate(),
+            })});
+
+            snapshot.data().situps
+            ?.forEach((stat: FirestoreStaticStat) => {
+              situpArrayTemp.push({
+                number: stat.number,
+                date: stat.date.toDate(),
+            })});
+
+            snapshot.data().runningData
+            ?.forEach((stat: FirestoreRunningStat) => {
+              runtimeArrayTemp.push({
+                timing: stat.timing,
+                date: stat.date.toDate(),
+                distance: stat.distance
+            })});
+
+            setPushupArray(pushupArrayTemp);
+            setSitupArray(situpArrayTemp);
+            setRuntimeArray(runtimeArrayTemp);
 
             let pushupRecent = snapshot
             .data()
@@ -53,8 +89,10 @@ export default function IpptCalculatorScreen() {
                 return b.date.toDate() === a.date.toDate(); // Sort by decreasing number of pushups per session
               }
             ).at(0);
+
             
-            pushupRecent = pushupRecent == undefined ? 0 : pushupRecent;
+            
+            pushupRecent = pushupRecent == undefined ? 0 : pushupRecent.number;
 
             let situpRecent = snapshot
             .data()
@@ -68,11 +106,11 @@ export default function IpptCalculatorScreen() {
               }
             ).at(0);
             
-            situpRecent = situpRecent == undefined ? 0 : situpRecent;
+            situpRecent = situpRecent.number == undefined ? 0 : situpRecent.number;
             
             let runtime = snapshot
             .data()
-            .situps?.sort(
+            .runningData?.sort(
               (a: FirestoreStaticStat, b: FirestoreStaticStat) => {
                 if (b.date.toDate() === a.date.toDate()) {
                   // If the score is the same, show the more recent one first
@@ -82,7 +120,7 @@ export default function IpptCalculatorScreen() {
               }
             ).at(0);
             
-            runtime = runtime == undefined ? 1200 : runtime;
+            runtime = runtime.timing == undefined ? 1100 : runtime.timing;
 
             let birthday: Date = snapshot.data().birthday?.toDate();
             const currDate : Date = new Date(); 
@@ -94,31 +132,89 @@ export default function IpptCalculatorScreen() {
             currDate.getDay() > birthday.getDay()) {
               yearsDiff--;
             }
+
+            if (yearsDiff > 60) {
+              yearsDiff = 60
+            } else if (yearsDiff < 18) {
+              yearsDiff = 18;
+            }
             
             // sort the values for pushup/situp/runtime by recency
-            setPushups(pushupRecent);
-            setSitups(situpRecent);
-            setRuntime(runtime);
+            setPushups(pushupRecent.toString());
+            setSitups(situpRecent.toString());
+            setRuntime(runtime.toString());
+            setRuntimeMins(Math.floor(parseInt(runtime) / 60).toString());
+            setRuntimeSecs((runtime % 60).toString());
             setAge(yearsDiff);
           }
         });
       }
-    };
+    }; 
     return unsubscribe();
   }, [user]);
 
   const colorScheme = useColorScheme();
 
   // create a new function to calculate score
-  const calculateScore = () => {
-    let pushUpScore = 0;
-    let sitUpScore = 0;
-    let runningScore = 0;
-    setTotalScore(0);
+  const exerciseScore = (table: any, stationVal: string, minVal: number, maxVal: number): number => {
+    if (parseInt(stationVal) > 99) {
+      stationVal = (Math.ceil(parseInt(stationVal) / 10) * 10).toString();
+    }
+    if (stationVal === "" || parseInt(stationVal) < minVal) {
+      return table[age.toString()][minVal.toString()];
+    } else if (parseInt(stationVal) > maxVal) {
+      return table[age.toString()][maxVal.toString()];
+    } else {
+      return table[age.toString()][parseInt(stationVal).toString()];
+    }
+    
   }
+
+  const overallScore = () => {
+    let pushUpScore = exerciseScore(pushupTable, pushups, 1, 60);
+    let sitUpScore = exerciseScore(situpTable, situps, 1, 60);
+    let runningScore = exerciseScore(runningTable, runtime, 510, 1100);
+    setTotalScore(pushUpScore + sitUpScore + runningScore);
+  }
+
+  useEffect(() => {
+    let runtimeSecsTemp = runtimeSecs === "" ? "0" : runtimeSecs;
+    let runtimeMinsTemp = runtimeMins === "" ? "0" : runtimeMins;
+    setRuntime((parseInt(runtimeMinsTemp) * 60 + parseInt(runtimeSecsTemp)).toString());
+    overallScore();
+  },[pushups, situps, runtimeMins, runtimeSecs, runtime])
+
+  const  saveEdits = () => {
+    let pushupArrayTemp2 = pushupArray;
+    let situpArrayTemp2 = situpArray;
+    let runtimeArrayTemp2 = runtimeArray;
+    console.log(pushupArrayTemp2)
+    pushupArrayTemp2.push({date: new Date(), number: parseInt(pushups)});
+    situpArrayTemp2.push({date: new Date(), number: parseInt(situps)});
+    runtimeArrayTemp2.push({date: new Date(), distance: 2400, timing: parseInt(runtime)});
+    const newData = {
+      pushups: pushupArrayTemp2 ?? null,
+      situps: situpArrayTemp2 ?? null,
+      runningData: runtimeArrayTemp2 ?? null,
+    };
+    const userProfileRef = doc(
+      firebaseFirestore,
+      "userProfiles",
+      currentUser!.uid
+    );
+    updateDoc(userProfileRef, newData)
+      .then(() => {
+        setSnackBarMessage("Changes saved successfully!");
+        setIsSnackBarVisible(true);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
 
   return (
     <ScrollView style={styles.container}>
+      <View>
       <CustomSnackBar
         type="success"
         visible={isSnackBarVisible}
@@ -133,14 +229,16 @@ export default function IpptCalculatorScreen() {
         label="Push-ups"
         keyboardType={'numeric'}
         maxLength={2}
-        onChangeText={() => {}}
+        value={pushups}
+        onChangeText={setPushups}
       />
       <CustomTextInput
         mode="flat"
         label="Sit-ups"
         keyboardType={'numeric'}
         maxLength={2}
-        onChangeText={() => {}}
+        value={situps}
+        onChangeText={setSitups}
       />
       <Card style={stylesRun.cardMain}>
       <Card.Title titleStyle={{color: Colors[colorScheme].tint, marginBottom:0, marginLeft: -10}}
@@ -150,16 +248,44 @@ export default function IpptCalculatorScreen() {
         mode="flat"
         label="mins"
         keyboardType={'numeric'}
-        onChangeText={() => {}}
+        maxLength={2}
+        value={runtimeMins}
+        onChangeText={setRuntimeMins}
       />
       <CustomTextInput style={[stylesRun.card, stylesRun.cardLeft]}
         mode="flat"
         label="secs"
         keyboardType={'numeric'}
-        onChangeText={() => {}}
+        maxLength={2}
+        value={runtimeSecs}
+        onChangeText={setRuntimeSecs}
       />
       </Card.Content>
       </Card>
+      <DataTable>
+      <DataTable.Header>
+        <DataTable.Title>Push-up</DataTable.Title>
+        <DataTable.Title numeric>Sit-up</DataTable.Title>
+        <DataTable.Title numeric>Run</DataTable.Title>
+        <DataTable.Title numeric>Overall</DataTable.Title>
+      </DataTable.Header>
+
+      <DataTable.Row>
+        <DataTable.Cell numeric>{exerciseScore(pushupTable, pushups, 1, 60)}</DataTable.Cell>
+        <DataTable.Cell numeric>{exerciseScore(situpTable, situps, 1, 60)}</DataTable.Cell>
+        <DataTable.Cell numeric>{exerciseScore(runningTable, runtime, 510, 1100)}</DataTable.Cell>
+        <DataTable.Cell numeric>{totalScore}</DataTable.Cell>
+      </DataTable.Row>
+      </DataTable>
+      </View>
+      <View>
+      </View>
+      <CustomButton
+              style={styles.button}
+              onPress={saveEdits}
+            >
+              Save Scores
+            </CustomButton>
     </ScrollView>
   );
 }
@@ -169,6 +295,9 @@ export default function IpptCalculatorScreen() {
 const styles = StyleSheet.create({
   topButtonContainer: {
     flexDirection: "row",
+  },
+  button: {
+    marginTop: 24,
   },
   radioButtonSet: {
     flexDirection: "row",
