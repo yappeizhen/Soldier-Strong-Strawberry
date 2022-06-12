3235307d0627859f85665808418ef11b6c819cbc
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 <66234273+yappeizhen@users.noreply.github.com>
2022-06-12 23:34:04 +0800

    Commit before merging solution

--- src/screens/ComputerVision/PushupCounter.js
+++ src/screens/ComputerVision/PushupCounter.js
@@ -5,23 +5,21 @@ import Svg, {
   Rect,
 } from 'react-native-svg';
 
-import { atan2 } from "@tensorflow/tfjs";
-
 import { LoadingView } from "./LoadingView";
 
 export function PushupCounter({ predictions }) {
   const [isGoingUp, setIsGoingUp] = useState(false);
   const [pushupCount, setPushupCount] = useState(0);
+  let isReady = false;
 
   const [feedback, setFeedback] = useState("");
   const [countStatus, setCountStatus] = useState("");
 
-  let angles = { elbow: 0, shoulder: 0, hip: 0, knee: 0, wrist: 0, ankle: 0 }
+  let angles = { elbow: 0, shoulder: 0, hip: 0, knee: 0 }
 
   if (!predictions) {
     return <LoadingView />
   }
-  console.log(predictions);
   //let nose = predictions.keypoints[0];
   //let leftEye = predictions.keypoints[1];
   //let rightEye = predictions.keypoints[2];
@@ -67,47 +65,50 @@ export function PushupCounter({ predictions }) {
     };
   };
   const findAngle = (first, second, third) => {
-    const center = getCoordinates(landmarkIndexMap[first]);
-    const p2 = getCoordinates(landmarkIndexMap[second]);
-    const p3 = getCoordinates(landmarkIndexMap[third]);
+    const B = getCoordinates(landmarkIndexMap[first]);
+    const A = getCoordinates(landmarkIndexMap[second]);
+    const C = getCoordinates(landmarkIndexMap[third]);
     // Each angle has the format {x, y}
-    const angle = Math.atan2(p3.y - center.y, p3.x - center.x) - atan2(p2.y - center.y, p2.x - center.x);
-    return Math.min(angle, 360 - angle);
+    let AB = Math.sqrt(Math.pow(B.x - A.x, 2) + Math.pow(B.y - A.y, 2));
+    let BC = Math.sqrt(Math.pow(B.x - C.x, 2) + Math.pow(B.y - C.y, 2));
+    let AC = Math.sqrt(Math.pow(C.x - A.x, 2) + Math.pow(C.y - A.y, 2));
+    return Math.acos((BC * BC + AB * AB - AC * AC) / (2 * BC * AB)) * (180 / Math.PI);
+    // const angle = Math.atan2(p3.y - center.y, p3.x - center.x) - atan2(p2.y - center.y, p2.x - center.x);
+    // return Math.min(angle, 360 - angle);
   }
   const isCorrectForm = () => {
     angles.elbow = findAngle("elbow", "wrist", "shoulder");
     angles.shoulder = findAngle("shoulder", "elbow", "hip");
     angles.hip = findAngle("hip", "shoulder", "knee");
     angles.knee = findAngle("knee", "hip", "ankle");
-    if (angles.elbow > 160
-      && angles.shoulder > 40
-      && angles.hip > 160 && angles.hip < 190
-      && angles.knee > 160) {
-      return true;
+    if (angles.elbow > 120
+      && angles.shoulder > 20
+      && angles.hip > 140 && angles.hip < 190
+      && angles.knee > 140) {
+      isReady = true;
+     // console.log("IM READYYYYYYYYY")
     }
-    return false;
   }
   const getPushUpStatus = () => {
-    if (isCorrectForm()) {
-      const elbowPercentage = (angles.elbow / 160) * 100;
-      if (elbowPercentage === 0) {
-        if (angles.elbow <= 90
-          && angles.hip > 160 && angles.hip < 190
-          && angles.knee > 160) {
+    if (isReady) {
+      if (angles.elbow < 100) {
+        if (angles.elbow <= 100
+          && angles.hip > 120 && angles.hip < 190
+          && angles.knee > 120) {
           setFeedback("Go Up");
-          if (!isGoingUp) { // TODO: check this
+          if (!isGoingUp) {
             setIsGoingUp(true);
             setCountStatus("");
           } else {
             setCountStatus("No Count");
           }
-        } else if (angles.elbow > 90) {
+        } else if (angles.elbow > 70) {
           setFeedback("Lower Body");
           setCountStatus("No Count!");
-        } else if (angles.hip <= 160 || hip >= 190) {
+        } else if (angles.hip <= 120 || angles.hip >= 190) {
           setFeedback("Straighten Back");
           setCountStatus("No Count");
-        } else if (knee <= 160) {
+        } else if (angles.knee <= 120) {
           setFeedback("Straighten Knee");
           setCountStatus("No Count");
         } else {
@@ -115,8 +116,8 @@ export function PushupCounter({ predictions }) {
           setCountStatus("No Count");
         }
       }
-      if (elbowPercentage === 100) {
-        if (angles.elbow > 160 && angles.shoulder > 40 && angles.hip > 160 && angles.hip < 190 && angles.knee > 160) {
+      if (angles.elbow > 120) {
+        if (angles.elbow > 120 && angles.shoulder > 40 && angles.hip > 120 && angles.hip < 190 && angles.knee > 120) {
           setCountStatus("");
           setFeedback("Go Down");
           if (isGoingUp) {
@@ -126,21 +127,24 @@ export function PushupCounter({ predictions }) {
           } else {
             setCountStatus("No Count")
           }
-        } else if (angles.elbow <= 160) {
+        } else if (angles.elbow <= 120) {
           setFeedback("Straighten Elbows");
           setCountStatus("No Count");
-        } else if (angles.hip <= 160 && angles.hip >= 190) {
+        } else if (angles.hip <= 120 && angles.hip >= 190) {
           setFeedback("Straighten Back");
           setCountStatus("No Count");
-        } else if (angles.knee <= 160) {
+        } else if (angles.knee <= 120) {
           setFeedback("Straighten Knee");
           setCountStatus("No Count");
         }
       }
     }
   }
-
+  if (!isReady) {
+    isCorrectForm();
+  }
   getPushUpStatus();
+  //console.log(angles);
 
   return (
     <>
