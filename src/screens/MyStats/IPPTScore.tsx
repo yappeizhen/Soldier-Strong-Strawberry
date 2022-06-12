1ef47c2295c3bb7bbda34cac5f0f0279ede5a844
tjunjet <latyun142@gmail.com>
2022-06-12 15:54:33 +0800

    Added most recent IPPT score

--- src/screens/MyStats/IPPTScore.tsx
+++ src/screens/MyStats/IPPTScore.tsx
@@ -1,3 +1,177 @@
+// import { doc, onSnapshot, updateDoc } from "firebase/firestore";
+// import { useState } from "react";
+// import { useEffect } from "react";
+// import { StyleSheet } from "react-native";
+// import { TouchableOpacity } from "react-native-gesture-handler";
+// import { Card, Checkbox, DataTable } from "react-native-paper";
+
+// import {
+//   AntDesign,
+//   Feather,
+//   Ionicons,
+//   MaterialCommunityIcons,
+// } from "@expo/vector-icons";
+
+// import CustomTextInput from "../../components/CustomInputText";
+// import { ScrollView, Text, View } from "../../components/Themed";
+// import Colors from "../../constants/Colors";
+// import { ScoreIPPT } from "../../constants/Models";
+// import { firebaseFirestore } from "../../firebase/firebase";
+// import { useAuthState } from "../../hooks/useAuthState";
+
+// export default function IPPTScore() {
+//   // ScoreIPPT is imported from Models
+//   const [score, setScore] = useState<ScoreIPPT>();
+//   const [newTrainingItemName, setNewTrainingItemName] = useState<number>(0);
+//   const [isTrainingDeleteMode, setIsTrainingDeleteMode] =
+//     useState<boolean>(false);
+//   const { user } = useAuthState();
+//   useEffect(() => {
+//     const unsubscribe = () => {
+//       if (user) {
+//         const userProfileRef = doc(firebaseFirestore, "userProfiles", user.uid);
+//         onSnapshot(userProfileRef, (snapshot) => {
+//           if (snapshot.exists()) {
+//             // Getting the data from firebase .data()
+//             setScore(snapshot.data().mostRecentIPPTScore);
+//           }
+//         });
+//       }
+//     };
+//     return unsubscribe();
+//   }, [user]);
+//     return (
+//       <View style={styles.container}>
+//         <Card style={styles.card}>
+//           <Card.Title
+//             title="Most Recent IPPT Score"
+//             subtitle="99"
+//           />
+//         </Card>
+//       </View>
+//   );
+// }
+
+// //   const addTrainingItem = (name: string) => {
+// //     const newItem = { item: name, isComplete: false };
+// //     const newscore = score;
+// //     newscore.push(newItem);
+// //     setScore(newscore);
+// //     const userProfileRef = doc(firebaseFirestore, "userProfiles", user!.uid);
+// //     updateDoc(userProfileRef, { score: score })
+// //       .then(() => {
+// //         console.log("New activity added successfully!");
+// //         setNewTrainingItemName("");
+// //       })
+// //       .catch((err) => {
+// //         console.log(err.message);
+// //       });
+// //   };
+// //   const removeTrainingItem = (index: number) => {
+// //     const newscore = score;
+// //     newscore.splice(index, 1);
+// //     setScore(newscore);
+// //     const userProfileRef = doc(firebaseFirestore, "userProfiles", user!.uid);
+// //     updateDoc(userProfileRef, { score: score })
+// //       .then(() => {
+// //         console.log("Item deleted successfully!");
+// //       })
+// //       .catch((err) => {
+// //         console.log(err.message);
+// //       });
+// //   };
+// //   return (
+// //       <View style={styles.container}>
+// //         <Card style={styles.card}>
+// //           <Card.Title
+// //             title = "Most Recent IPPT Score"
+// //             right={() => {
+// //               if (!isTrainingDeleteMode) {
+// //                 return (
+// //                   <TouchableOpacity
+// //                     style={{ marginRight: 24 }}
+// //                     onPress={() => setIsTrainingDeleteMode(true)}
+// //                   >
+// //                     <AntDesign name="setting" size={24} color="black" />
+// //                   </TouchableOpacity>
+// //                 );
+// //               } else {
+// //                 return (
+// //                   <TouchableOpacity
+// //                     style={{ marginRight: 24 }}
+// //                     onPress={() => setIsTrainingDeleteMode(false)}
+// //                   >
+// //                     <Feather name="save" size={24} color="black" />
+// //                   </TouchableOpacity>
+// //                 );
+// //               }
+// //             }}
+// //           />
+// //           <Card.Content>
+// //             {renderScore()}
+// //             <DataTable.Row
+// //               style={{
+// //                 flexDirection: "row",
+// //                 alignItems: "center",
+// //               }}
+// //             >
+// //               <View
+// //                 style={{
+// //                   flexDirection: "row",
+// //                   alignItems: "center",
+// //                 }}
+// //               >
+// //                 <CustomTextInput
+// //                   mode="outlined"
+// //                   value={newTrainingItemName}
+// //                   onChangeText={setNewTrainingItemName}
+// //                   style={{ padding: 0, height: 32, width: "85%" }}
+// //                 />
+// //                 <TouchableOpacity
+// //                   onPress={() => {
+// //                     if (newTrainingItemName) {
+// //                       addTrainingItem(newTrainingItemName);
+// //                     }
+// //                   }}
+// //                   disabled={!newTrainingItemName}
+// //                 >
+// //                   <Ionicons
+// //                     name="add-circle-outline"
+// //                     size={28}
+// //                     color={
+// //                       newTrainingItemName ? Colors["light"].tint : "lightgrey"
+// //                     }
+// //                     style={{ margin: 12 }}
+// //                   />
+// //                 </TouchableOpacity>
+// //               </View>
+// //             </DataTable.Row>
+// //           </Card.Content>
+// //         </Card>
+// //       </View>
+// //   );
+// // }
+
+// const styles = StyleSheet.create({
+//   button: {
+//     marginTop: 24,
+//   },
+//   card: {
+//     width: "80%",
+//     marginTop: 20,
+//   },
+//   container: {
+//     flex: 1,
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+//   title: {
+//     marginTop: 24,
+//     fontSize: 20,
+//     fontWeight: "bold",
+//   },
+// });
+
 import { doc, onSnapshot, updateDoc } from "firebase/firestore";
 import { useState } from "react";
 import { useEffect } from "react";
@@ -38,7 +212,8 @@ export default function IPPTScore() {
     };
     return unsubscribe();
   }, [user]);
-  const renderScore = () => {
+
+const renderScore = () => {
     let displayedscore = score;
     return (
       <DataTable>
@@ -162,7 +337,8 @@ export default function IPPTScore() {
                 }}
               >
                 <CustomTextInput
-                  mode="outlined"
+
+mode="outlined"
                   value={newTrainingItemName}
                   onChangeText={setNewTrainingItemName}
                   style={{ padding: 0, height: 32, width: "85%" }}
@@ -194,11 +370,11 @@ export default function IPPTScore() {
 
 const styles = StyleSheet.create({
   button: {
-    marginTop: 24,
+    marginBottom: 24,
   },
   card: {
     width: "80%",
-    marginTop: 20,
+    marginBottom: 24
   },
   container: {
     flex: 1,
