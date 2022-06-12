aff5ad56749007496e179c493f38b40f7fbd123a
tjunjet <latyun142@gmail.com>
2022-06-12 14:26:57 +0800

    Added Most Recent IPPT Score

--- src/screens/MyStats/IPPTScore.tsx
+++ src/screens/MyStats/IPPTScore.tsx
@@ -15,14 +15,15 @@ import {
 import CustomTextInput from "../../components/CustomInputText";
 import { ScrollView, Text, View } from "../../components/Themed";
 import Colors from "../../constants/Colors";
-import { IPPTScore } from "../../constants/Models";
+import { ScoreIPPT } from "../../constants/Models";
 import { firebaseFirestore } from "../../firebase/firebase";
 import { useAuthState } from "../../hooks/useAuthState";
 
-// To show most recent IPPT Score
-
 export default function IPPTScore() {
-  const [score, setScore] = useState<IPPTScore[]>([]);
+  const [score, setScore] = useState<ScoreIPPT[]>([]);
+  const [newTrainingItemName, setNewTrainingItemName] = useState<string>("");
+  const [isTrainingDeleteMode, setIsTrainingDeleteMode] =
+    useState<boolean>(false);
   const { user } = useAuthState();
   useEffect(() => {
     const unsubscribe = () => {
@@ -38,10 +39,10 @@ export default function IPPTScore() {
     return unsubscribe();
   }, [user]);
   const renderScore = () => {
-    let displayedTrainingPlan = score;
+    let displayedscore = score;
     return (
       <DataTable>
-        {displayedTrainingPlan.map((item, index) => {
+        {displayedscore.map((item, index) => {
           return (
             <DataTable.Row key={index}>
               <DataTable.Cell style={{ flex: 12 }}>
@@ -54,14 +55,14 @@ export default function IPPTScore() {
                     status={item.isComplete ? "checked" : "unchecked"}
                     onPress={() => {
                       item.isComplete = !item.isComplete;
-                      setTrainingPlan(displayedTrainingPlan);
+                      setScore(displayedscore);
                       const userProfileRef = doc(
                         firebaseFirestore,
                         "userProfiles",
                         user!.uid
                       );
                       updateDoc(userProfileRef, {
-                        trainingPlan: trainingPlan,
+                        score: score,
                       })
                         .then(() => {
                           console.log("Checkbox updated successfully!");
@@ -91,6 +92,105 @@ export default function IPPTScore() {
       </DataTable>
     );
   };
+  const addTrainingItem = (name: string) => {
+    const newItem = { item: name, isComplete: false };
+    const newscore = score;
+    newscore.push(newItem);
+    setScore(newscore);
+    const userProfileRef = doc(firebaseFirestore, "userProfiles", user!.uid);
+    updateDoc(userProfileRef, { score: score })
+      .then(() => {
+        console.log("New activity added successfully!");
+        setNewTrainingItemName("");
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+  const removeTrainingItem = (index: number) => {
+    const newscore = score;
+    newscore.splice(index, 1);
+    setScore(newscore);
+    const userProfileRef = doc(firebaseFirestore, "userProfiles", user!.uid);
+    updateDoc(userProfileRef, { score: score })
+      .then(() => {
+        console.log("Item deleted successfully!");
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+  return (
+      <View style={styles.container}>
+        <Card style={styles.card}>
+          <Card.Title
+            title = "Most Recent IPPT Score"
+            right={() => {
+              if (!isTrainingDeleteMode) {
+                return (
+                  <TouchableOpacity
+                    style={{ marginRight: 24 }}
+                    onPress={() => setIsTrainingDeleteMode(true)}
+                  >
+                    <AntDesign name="setting" size={24} color="black" />
+                  </TouchableOpacity>
+                );
+              } else {
+                return (
+                  <TouchableOpacity
+                    style={{ marginRight: 24 }}
+                    onPress={() => setIsTrainingDeleteMode(false)}
+                  >
+                    <Feather name="save" size={24} color="black" />
+                  </TouchableOpacity>
+                );
+              }
+            }}
+          />
+          <Card.Content>
+            {renderScore()}
+            <DataTable.Row
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+              }}
+            >
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                <CustomTextInput
+                  mode="outlined"
+                  value={newTrainingItemName}
+                  onChangeText={setNewTrainingItemName}
+                  style={{ padding: 0, height: 32, width: "85%" }}
+                />
+                <TouchableOpacity
+                  onPress={() => {
+                    if (newTrainingItemName) {
+                      addTrainingItem(newTrainingItemName);
+                    }
+                  }}
+                  disabled={!newTrainingItemName}
+                >
+                  <Ionicons
+                    name="add-circle-outline"
+                    size={28}
+                    color={
+                      newTrainingItemName ? Colors["light"].tint : "lightgrey"
+                    }
+                    style={{ margin: 12 }}
+                  />
+                </TouchableOpacity>
+              </View>
+            </DataTable.Row>
+          </Card.Content>
+        </Card>
+      </View>
+  );
+}
 
 const styles = StyleSheet.create({
   button: {
