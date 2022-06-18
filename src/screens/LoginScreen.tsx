d8ab297fe40802f11549db12e5b57769ab08489e
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 <66234273+yappeizhen@users.noreply.github.com>
2022-06-18 23:33:04 +0800

    Added logo

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,11 +1,12 @@
 import { signInWithEmailAndPassword } from "firebase/auth";
 import React from "react";
 import { useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { Image, StyleSheet, View } from "react-native";
 
 import CustomButton from "../components/CustomButton";
 import CustomTextInput from "../components/CustomInputText";
 import CustomSnackBar from "../components/CustomSnackBar";
+import { ScrollView } from "../components/Themed";
 import Colors from "../constants/Colors";
 import { firebaseAuth } from "../firebase/firebase";
 import useColorScheme from "../hooks/useColorScheme";
@@ -41,7 +42,21 @@ export default function LoginScreen({ navigation }: any) {
   };
 
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <ScrollView style={{ flex: 1 }}>
+      <View
+        style={{
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+          padding: 40,
+          marginTop: "10%",
+        }}
+      >
+        <Image
+          style={{ height: 260, width: 260 }}
+          source={require("../assets/logos/soldierStrongLogo.png")}
+        />
+      </View>
       <View
         style={{
           flex: 0.3,
@@ -91,7 +106,7 @@ export default function LoginScreen({ navigation }: any) {
         type="error"
         message={snackMessage}
       ></CustomSnackBar>
-    </View>
+    </ScrollView>
   );
 }
 
